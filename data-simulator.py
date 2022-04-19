0f08d45dd247f20e3cb4957708c98c2064acdadd
natividadesusana <susanajdsn@gmail.com>
2022-04-19 18:19:11 +0100

    creating interface

--- data-simulator.py
+++ data-simulator.py
@@ -1,12 +1,20 @@
 # Simulador de dado
 # Simulando o uso de um dado, gerando um valor de 1 até 6
 import random
+import PySimpleGUI 
 
 class DataSimulator:
     def __init__(self):
         self.minimum_value = 1
         self.maximum_value = 6
         self.message = 'Generate new value for data [Y/N]? '
+        # Layout
+        layout = [
+            [sg.Text('Roll the dice?')],
+            [sg.Buttom('Yes'),sg.Button('No')]
+        ]
+        # Criando uma janela
+        window = sg.Window('Data Simulator', Layout=layout)
 
     def Start(self):
         answer = input(self.message)
