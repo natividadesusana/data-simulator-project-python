c9ce429847c85829ce58b61c450f5134c53623a4
natividadesusana <susanajdsn@gmail.com>
2022-04-19 22:53:13 +0100

    adding messages in the interface

--- data-simulator.py
+++ data-simulator.py
@@ -12,6 +12,20 @@ class DataSimulator:
             [sg.Text('Roll the dice?')],
             [sg.Button('yes'),sg.Button('no')]
         ]
+        self.layout2 = [
+            [sg.Text('Your number is:')],
+            [sg.Text(self.GenerateDataValue())],
+            [sg.Button('Roll the dice again'), sg.Button('Finish')]
+        ]
+        self.finish_message = [
+            [sg.Text('We appreciate your participation!')]
+        ]
+        self.choice_message = [
+            [sg.Text('Choose an option [YES/NO]!')]
+        ]
+        self.erro_message = [
+            [sg.Text('There was an error receiving your reply!')]
+        ]
 
     def Start(self):
         # Criando uma janela
@@ -21,16 +35,40 @@ class DataSimulator:
         # Gerando alguma coisa com os valores
         try:
             if self.events == 'yes' or self.events == 'y':
-                self.GenerateDataValue()
+                return self.PlayAgain()
             elif self.events == 'no' or self.events == 'n':
-                print('We appreciate your participation!')
+                return self.FinishMessage()
             else:
-                print('Please type [yes or no]!')
+                return self.ChoiceMessage()
         except:
-                print('There was an error receiving your reply!')
+                return self.ErroMessage()
 
     def GenerateDataValue(self):
-        print(random.randint(self.minimum_value, self.maximum_value))
+        return random.randint(self.minimum_value, self.maximum_value)
+
+    def PlayAgain(self):
+        self.window = sg.Window('Data Simulator', layout=self.layout2)
+        self.window.Read()
+        try:
+            self.layout2
+            if self.events == 'Roll the dice again':
+                print('ok')
+            else:
+                return self.FinishMessage()
+        except:
+            print('There was an error receiving your reply!')
+
+    def FinishMessage(self):
+        self.window = sg.Window('Data Simulator', layout=self.finish_message)
+        self.window.Read()
+
+    def ChoiceMessage(self):
+        self.window = sg.Window('Data Simulator', layout=self.choice_message)
+        self.window.Read()
+
+    def ErroMessage(self):
+        self.window = sg.Window('Data Simulator', layout=self.erro_message)
+        self.window.Read()
 
 simulator = DataSimulator()
 simulator.Start()
