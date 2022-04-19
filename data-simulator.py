3ef9a618a3f2ddce9eb7f2fb9d986468461918a7
natividadesusana <susanajdsn@gmail.com>
2022-04-19 20:45:40 +0100

    creating interface

--- data-simulator.py
+++ data-simulator.py
@@ -1,37 +1,38 @@
 # Simulador de dado
 # Simulando o uso de um dado, gerando um valor de 1 até 6
 import random
-import PySimpleGUI 
+import PySimpleGUI as sg 
 
 class DataSimulator:
     def __init__(self):
         self.minimum_value = 1
         self.maximum_value = 6
-        self.message = 'Generate new value for data [Y/N]? '
         # Layout
-        layout = [
+        self.layout = [
             [sg.Text('Roll the dice?')],
-            [sg.Buttom('Yes'),sg.Button('No')]
+            [sg.Button('yes'),sg.Button('no')]
         ]
-        # Criando uma janela
-        window = sg.Window('Data Simulator', Layout=layout)
 
     def Start(self):
-        answer = input(self.message)
+        # Criando uma janela
+        self.window = sg.Window('Data Simulator', layout=self.layout)
+        # Lendo os valores da tela
+        self.events, self.values = self.window.Read()
+        # Gerando alguma coisa com os valores
         try:
-            if answer == 'yes' or answer == 'y':
+            if self.events == 'yes' or self.events == 'y':
                 self.GenerateDataValue()
-            elif answer == 'no' or answer == 'n':
+            elif self.events == 'no' or self.events == 'n':
                 print('We appreciate your participation!')
             else:
                 print('Please type [yes or no]!')
         except:
-            print('There was an error receiving your reply!')
+                print('There was an error receiving your reply!')
 
     def GenerateDataValue(self):
         print(random.randint(self.minimum_value, self.maximum_value))
 
-simulador = DataSimulator()
-simulador.Start()
+simulator = DataSimulator()
+simulator.Start()
 
     
